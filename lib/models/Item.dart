289ef1495b5c168be0014a4d13e2fa215230f966
seen2 <tiwarietc@gmail.com>
2021-06-21 02:38:00 +0530

    List rendering and null safet with assert

--- lib/models/Item.dart
+++ lib/models/Item.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 class Item {
   final String id;
   final String name;
@@ -17,13 +15,15 @@ class Item {
       this.image = ''});
 }
 
-final products = [
-  Item(
-      id: "Codepur001",
-      name: "iPhone 12 Pro",
-      desc: "Apple iPhone 12th generation",
-      price: 999,
-      color: "#33505a",
-      image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
-];
+class CatalogMoodel {
+  static final items = [
+    Item(
+        id: "Codepur001",
+        name: "iPhone 12 Pro",
+        desc: "Apple iPhone 12th generation",
+        price: 999,
+        color: "#33505a",
+        image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+  ];
+}
--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/models/Item.dart';
 
 import 'package:hello_world/screens/DrawerScreen.dart';
+import 'package:hello_world/widgets/ItemWidget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogMoodel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-          child: Container(
-        child: Text("Welcome to 30 days of flutter"),
-      )),
+      body: ListView.builder(
+        itemCount: dummyList.length,
+        itemBuilder: (context, index) {
+          return ItemWidget(item: dummyList[index]);
+        },
+      ),
       drawer: DrawerScreen(),
     );
   }
